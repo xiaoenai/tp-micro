94af408ce8568f68708c3a97c5f03910a8b072e2
henrylee2cn <henrylee_cn@foxmail.com>
2018-03-08 15:07:05 +0800

    Config: Complete antconfig server command

---
+++ config/cmd/antconfig/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/henrylee2cn/ant"
+	"github.com/henrylee2cn/ant/discovery"
+	"github.com/henrylee2cn/ant/discovery/etcd"
+	"github.com/henrylee2cn/cfgo"
+	conf "github.com/xiaoenai/ants/config"
+)
+
+type config struct {
+	Server ant.SrvConfig   `yaml:"server"`
+	Etcd   etcd.EasyConfig `yaml:"etcd"`
+}
+
+func (e *config) Reload(bindFunc cfgo.BindFunc) error {
+	return bindFunc()
+}
+
+func main() {
+	cfg := &config{
+		Server: ant.SrvConfig{
+			ListenAddress: ":4040",
+		},
+		Etcd: etcd.EasyConfig{
+			Endpoints: []string{"http://127.0.0.1:2379"},
+		},
+	}
+	cfgo.MustReg("antconfig", cfg)
+
+	etcdClient, err := etcd.EasyNew(cfg.Etcd)
+	if err != nil {
+		ant.Fatalf("%v", err)
+	}
+
+	conf.InitMgr(etcdClient)
+
+	srv := ant.NewServer(
+		cfg.Server,
+		discovery.ServicePluginFromEtcd(
+			cfg.Server.InnerIpPort(),
+			etcdClient,
+		),
+	)
+	srv.RoutePull(conf.PullCtrl())
+	srv.Listen()
+}
