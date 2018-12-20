2bf90af1101c73ff9d23e8ee9db16d9f41862cfd
henrylee2cn <henrylee2cn@gmail.com>
2018-12-20 17:53:47 +0800

    fix: watchOffline

--- circuit_breaker.go
+++ circuit_breaker.go
@@ -198,7 +198,7 @@ func (c *circuitBreaker) close() {
 
 func (c *circuitBreaker) watchOffline() {
 	ch := c.linker.WatchOffline()
-	for addr := range <-ch {
+	for addr := range ch {
 		_s, ok := c.sessLib.Load(addr)
 		if !ok {
 			continue
