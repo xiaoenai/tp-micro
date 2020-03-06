74612b24c59dd835178effde84fdf690960efac5
swxctx <swxctxone@yeah.net>
2020-03-06 21:41:53 +0800

    v6.1.1->v6.1.2

--- micro/create/tpl.go
+++ micro/create/tpl.go
@@ -981,6 +981,6 @@ require (
 	github.com/henrylee2cn/cfgo v0.0.0-20180417024816-e6c3cc325b21
 	github.com/henrylee2cn/erpc/v6 v6.3.1
 	github.com/henrylee2cn/goutil v0.0.0-20191202093501-834eaf50f6fe
-	github.com/xiaoenai/tp-micro/v6 v6.1.1
+	github.com/xiaoenai/tp-micro/v6 v6.1.2
 )
 `
